2b00425da62ddcc23695afdfae32e7792d52bf7a
Zach McWhirter <zmac7811@gmail.com>
2021-02-11 18:01:26 -0600

    componentDidMount

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,27 +6,16 @@ class App extends Component {
     super();
     
     this.state = {
-      monsters: [
-        {
-          name: "Mummy",
-          id: "mummy"
-        },
-        {
-          name: "Dracula",
-          id: "drac"
-        },
-        {
-          name: "Werewolf",
-          id: "wolf"
-        },
-        {
-          name: "Zombie",
-          id: "zombie"
-        }
-      ]
-    }
-  }
-  
+      monsters: []
+    };
+  };
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(res => res.json())
+      .then(users => this.setState({ monsters: users}));
+  };
+
   render() {
     return (
       <div className="App">
