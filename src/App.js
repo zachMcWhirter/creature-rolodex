824a6a27ccd92bd5f48ee4894c66559d0dcc1df8
Zach McWhirter <zmac7811@gmail.com>
2021-02-28 16:57:26 -0600

    add email and basic styling

--- src/App.js
+++ src/App.js
@@ -20,14 +20,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <CardList name='Bruce Liam'>
-        {this.state.monsters.map(monster => (
-          <h1 key={monster.id}> {monster.name} </h1>
-        ))}
+        <CardList monsters={this.state.monsters}>
         </CardList>
       </div>
     );
-  }
-}
+  }       
+}   
 
 export default App;
---
+++ src/components/card/card.component.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './card-component.styles.css';
+
+export const Card = (props) => (
+  <div className='card-container'>
+
+    {/* Passing in the ${props.monster.id} allows robohash to generate a unique image for each user based on the id of that user */}
+    <img 
+      alt='monster' 
+      src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`} 
+    />
+    <h2> {props.monster.name} </h2>
+    <p> {props.monster.email} </p>
+  </div>
+)
