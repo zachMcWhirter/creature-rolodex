27caf748933916a3098248b8341a973eeaab50ad
Zach McWhirter <zmac7811@gmail.com>
2021-03-01 14:20:40 -0600

    setup search feature part 1

--- src/components/card/card.component.js
+++ src/components/card/card.component.js
@@ -5,7 +5,9 @@ import './card-component.styles.css';
 export const Card = (props) => (
   <div className='card-container'>
 
-    {/* Passing in the ${props.monster.id} allows robohash to generate a unique image for each user based on the id of that user */}
+    {/* Passing in the ${props.monster.id} allows 
+    robohash to generate a unique image for each 
+    user based on the id of that user */}
     <img 
       alt='monster' 
       src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`} 
