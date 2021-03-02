d56a2e07aa1980ccd02e9da3dd3f8e87638bf7e9
Zach McWhirter <zmac7811@gmail.com>
2021-03-02 11:16:34 -0600

    refactor and final styling

--- src/App.js
+++ src/App.js
@@ -18,6 +18,10 @@ class App extends Component {
     .then(res => res.json())
     .then(users => this.setState({ monsters: users}));
   }
+
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  }
   
   render() {
 
@@ -35,10 +39,10 @@ class App extends Component {
 
     return (
       <div className="App">
+        <h1> Monsters Rolodex </h1>
         <SearchBox 
           placeholder='search monsters'
-          handleChange={e => 
-            this.setState({ searchField: e.target.value })}
+          handleChange={this.handleChange}
         />
           {/* Pass in filteredMonsters to generate the filtered list */}
         <CardList monsters={filteredMonsters}>
